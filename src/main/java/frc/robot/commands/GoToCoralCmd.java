1e4f9b6888f611a1c168cb90588f4f0b32e0b08c
程式浪人 <raymondma529@foxmail.com>
2025-07-05 23:08:40 +0800

    re-format Code

--- src/main/java/frc/robot/commands/GoToCoralCmd.java
+++ src/main/java/frc/robot/commands/GoToCoralCmd.java
@@ -17,79 +17,84 @@ import frc.robot.Constants;
 
 /** An example command that uses an example subsystem. */
 public class GoToCoralCmd extends Command {
-  private final CommandSwerveDrivetrain m_subsystem;
-  private Pose2d currentCoral = new Pose2d(-1, -1, new Rotation2d(3));
-  private int flew = 0;
-  public GoToCoralCmd(CommandSwerveDrivetrain subsystem) {
-    m_subsystem = subsystem;
-    addRequirements(subsystem);
-  }
-  public Pose2d getTargetPose2d() {
-    String coralLimeLight = Constants.LIME_LIGHT_OBJECT_DETECTION;
-    double lh = 0.922; // ll to ground height
-    double la = Math.toRadians(35); // ll angle against the wall
-    double tx = Math.toRadians(LimelightHelpers.getTX(coralLimeLight));
-    double ty = Math.toRadians(LimelightHelpers.getTY(coralLimeLight));
-    double lg1 = Math.tan(Math.PI/2-la+ty) * lh;
-    double lg2 = Math.sqrt(Math.pow(lg1, 2)+Math.pow(lh, 2)) * Math.tan(tx);
-    double da = Math.sqrt(Math.pow(lg1, 2)+Math.pow(lg2, 2))*0.6; // direct distance
-    Pose2d robotPose = m_subsystem.getPose();
-    double robotAngle = m_subsystem.getPose().getRotation().getRadians();
-    double Cx = robotPose.getX() + Math.cos(robotAngle-Math.PI) * da;
-    double Cy = robotPose.getY() + Math.sin(robotAngle-Math.PI) * da;
-    Rotation2d Cr = new Rotation2d(robotAngle - tx*2);
-    Pose2d coralPose2d = new Pose2d(Cx, Cy, Cr);
-    return coralPose2d;
-  }
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+    private final CommandSwerveDrivetrain m_subsystem;
+    private Pose2d currentCoral = new Pose2d(-1, -1, new Rotation2d(3));
+    private int flew = 0;
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    String coralLimelight = Constants.LIME_LIGHT_OBJECT_DETECTION;
-    boolean hasTarget = LimelightHelpers.getTV(coralLimelight);
-    SmartDashboard.putBoolean("ll hasT", hasTarget);
-    int didFlyTolerance = 10;
-    if (!hasTarget) {
-      flew += 1;
-      if (flew >= didFlyTolerance) {
-        System.out.println("flewn");
-        m_subsystem.customStopMoving(true);
-        // find new coral here
-        return;
-      }
+    public GoToCoralCmd(CommandSwerveDrivetrain subsystem) {
+        m_subsystem = subsystem;
+        addRequirements(subsystem);
     }
-    else {
-      flew = 0;
-    }
-    Pose2d tempCoralPose = getTargetPose2d();
-    // SmartDashboard.putNumber("coral angle", tempCoralPose.getRotation().getDegrees());
-    // SmartDashboard.putNumber("current angle", m_subsystem.getPose().getRotation().getDegrees());
-    double distanceTolerance = 0.5;
-    double eucDistance = tempCoralPose.getTranslation().getDistance(currentCoral.getTranslation());
-    // SmartDashboard.putNumber("current coral X", currentCoral.getX());
-    // SmartDashboard.putNumber("Target Distance", eucDistance);
-    if (eucDistance < distanceTolerance) {
-      System.out.println("no change");
-      m_subsystem.autoMoveToPose(currentCoral);
-      return;
-    }
-    else {
-      System.out.println("change");
-      currentCoral = tempCoralPose;
-    }
-    m_subsystem.autoMoveToPose(currentCoral);
-  }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+    public Pose2d getTargetPose2d() {
+        String coralLimeLight = Constants.LIME_LIGHT_OBJECT_DETECTION;
+        double lh = 0.922; // ll to ground height
+        double la = Math.toRadians(35); // ll angle against the wall
+        double tx = Math.toRadians(LimelightHelpers.getTX(coralLimeLight));
+        double ty = Math.toRadians(LimelightHelpers.getTY(coralLimeLight));
+        double lg1 = Math.tan(Math.PI / 2 - la + ty) * lh;
+        double lg2 = Math.sqrt(Math.pow(lg1, 2) + Math.pow(lh, 2)) * Math.tan(tx);
+        double da = Math.sqrt(Math.pow(lg1, 2) + Math.pow(lg2, 2)) * 0.6; // direct distance
+        Pose2d robotPose = m_subsystem.getPose();
+        double robotAngle = m_subsystem.getPose().getRotation().getRadians();
+        double Cx = robotPose.getX() + Math.cos(robotAngle - Math.PI) * da;
+        double Cy = robotPose.getY() + Math.sin(robotAngle - Math.PI) * da;
+        Rotation2d Cr = new Rotation2d(robotAngle - tx * 2);
+        Pose2d coralPose2d = new Pose2d(Cx, Cy, Cr);
+        return coralPose2d;
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        String coralLimelight = Constants.LIME_LIGHT_OBJECT_DETECTION;
+        boolean hasTarget = LimelightHelpers.getTV(coralLimelight);
+        SmartDashboard.putBoolean("ll hasT", hasTarget);
+        int didFlyTolerance = 10;
+        if (!hasTarget) {
+            flew += 1;
+            if (flew >= didFlyTolerance) {
+                System.out.println("flewn");
+                m_subsystem.customStopMoving(true);
+                // find new coral here
+                return;
+            }
+        } else {
+            flew = 0;
+        }
+        Pose2d tempCoralPose = getTargetPose2d();
+        // SmartDashboard.putNumber("coral angle",
+        // tempCoralPose.getRotation().getDegrees());
+        // SmartDashboard.putNumber("current angle",
+        // m_subsystem.getPose().getRotation().getDegrees());
+        double distanceTolerance = 0.5;
+        double eucDistance = tempCoralPose.getTranslation().getDistance(currentCoral.getTranslation());
+        // SmartDashboard.putNumber("current coral X", currentCoral.getX());
+        // SmartDashboard.putNumber("Target Distance", eucDistance);
+        if (eucDistance < distanceTolerance) {
+            System.out.println("no change");
+            m_subsystem.autoMoveToPose(currentCoral);
+            return;
+        } else {
+            System.out.println("change");
+            currentCoral = tempCoralPose;
+        }
+        m_subsystem.autoMoveToPose(currentCoral);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
