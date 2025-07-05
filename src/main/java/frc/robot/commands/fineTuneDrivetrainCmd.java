256d28bf1abef488a0cd4e440bea71d406330a70
程式浪人 <raymondma529@foxmail.com>
2025-07-05 23:14:30 +0800

    re-format codes

--- src/main/java/frc/robot/commands/fineTuneDrivetrainCmd.java
+++ src/main/java/frc/robot/commands/fineTuneDrivetrainCmd.java
@@ -11,49 +11,53 @@ import frc.robot.subsystems.Chassis.CommandSwerveDrivetrain;
 
 /** An example command that uses an example subsystem. */
 public class fineTuneDrivetrainCmd extends Command {
-  private final CommandSwerveDrivetrain m_subsystem;
-  private int direction;
-  // direction: 0 up 1 left 2 down 3 right
-  public fineTuneDrivetrainCmd(CommandSwerveDrivetrain subsystem, int povDirection) {
-    m_subsystem = subsystem;
-    addRequirements(subsystem);
-    direction = povDirection;
-  }
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+    private final CommandSwerveDrivetrain m_subsystem;
+    private int direction;
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    // double x_speed = (direction == 1 || direction == 3) ? 0.5 : 0;
-    // double y_speed = (direction == 0 || direction == 2) ? 0.5 : 0;
-    double x_speed = 0;
-    double y_speed = 0;
-    if (direction == 0) {
-      x_speed = 0.5;
+    // direction: 0 up 1 left 2 down 3 right
+    public fineTuneDrivetrainCmd(CommandSwerveDrivetrain subsystem, int povDirection) {
+        m_subsystem = subsystem;
+        addRequirements(subsystem);
+        direction = povDirection;
     }
-    if (direction == 2) {
-      x_speed = -0.5;
-    }
-    if (direction == 1) {
-      y_speed = 0.5;
-    }
-    if (direction == 3) {
-      y_speed = -0.5;
-    }
-    SmartDashboard.putBoolean("min moving", true);
-    m_subsystem.customMoveWithSpeed(x_speed, y_speed);
-    SmartDashboard.putBoolean("min moving", false);
-  }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        // double x_speed = (direction == 1 || direction == 3) ? 0.5 : 0;
+        // double y_speed = (direction == 0 || direction == 2) ? 0.5 : 0;
+        double x_speed = 0;
+        double y_speed = 0;
+        if (direction == 0) {
+            x_speed = 0.5;
+        }
+        if (direction == 2) {
+            x_speed = -0.5;
+        }
+        if (direction == 1) {
+            y_speed = 0.5;
+        }
+        if (direction == 3) {
+            y_speed = -0.5;
+        }
+        SmartDashboard.putBoolean("min moving", true);
+        m_subsystem.customMoveWithSpeed(x_speed, y_speed);
+        SmartDashboard.putBoolean("min moving", false);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
--- src/main/java/frc/robot/subsystems/GroundIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/GroundIntakeSubsystem.java
@@ -16,76 +16,76 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.GIntakeConstants;
 
 public class GroundIntakeSubsystem extends SubsystemBase {
-  private final TalonFX m_turnMotor = new TalonFX(GIntakeConstants.GIntakeTurnID);
-  private final TalonFX m_driveMotor = new TalonFX(GIntakeConstants.GIntakeDriveID);
-  private final CANcoder m_CANcoder = new CANcoder(GIntakeConstants.GIntakeCCID);
+    private final TalonFX m_turnMotor = new TalonFX(GIntakeConstants.GIntakeTurnID);
+    private final TalonFX m_driveMotor = new TalonFX(GIntakeConstants.GIntakeDriveID);
+    private final CANcoder m_CANcoder = new CANcoder(GIntakeConstants.GIntakeCCID);
 
-  private TalonFXConfiguration turnMotorConfiguration () {
-    TalonFXConfiguration turnMotorConfig = new TalonFXConfiguration();
-    turnMotorConfig.Slot0.kP = GIntakeConstants.turnMotorConst.kP;
-    turnMotorConfig.Slot0.kI = GIntakeConstants.turnMotorConst.kI;
-    turnMotorConfig.Slot0.kD = GIntakeConstants.turnMotorConst.kD;
-    turnMotorConfig.Slot0.kS = GIntakeConstants.turnMotorConst.kS;
-    turnMotorConfig.Slot0.kV = GIntakeConstants.turnMotorConst.kV;
-    turnMotorConfig.Slot0.kA = GIntakeConstants.turnMotorConst.kA;
-    turnMotorConfig.Feedback.FeedbackRemoteSensorID = GIntakeConstants.GIntakeCCID;
-    turnMotorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
-    turnMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-    return turnMotorConfig;
-  }
+    private TalonFXConfiguration turnMotorConfiguration() {
+        TalonFXConfiguration turnMotorConfig = new TalonFXConfiguration();
+        turnMotorConfig.Slot0.kP = GIntakeConstants.turnMotorConst.kP;
+        turnMotorConfig.Slot0.kI = GIntakeConstants.turnMotorConst.kI;
+        turnMotorConfig.Slot0.kD = GIntakeConstants.turnMotorConst.kD;
+        turnMotorConfig.Slot0.kS = GIntakeConstants.turnMotorConst.kS;
+        turnMotorConfig.Slot0.kV = GIntakeConstants.turnMotorConst.kV;
+        turnMotorConfig.Slot0.kA = GIntakeConstants.turnMotorConst.kA;
+        turnMotorConfig.Feedback.FeedbackRemoteSensorID = GIntakeConstants.GIntakeCCID;
+        turnMotorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
+        turnMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        return turnMotorConfig;
+    }
 
-  private TalonFXConfiguration driveMotorConfiguration () {
-    TalonFXConfiguration driveMotorConfig = new TalonFXConfiguration();
-    driveMotorConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-    return driveMotorConfig;
-  }
+    private TalonFXConfiguration driveMotorConfiguration() {
+        TalonFXConfiguration driveMotorConfig = new TalonFXConfiguration();
+        driveMotorConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        return driveMotorConfig;
+    }
 
-  public void configGroundIntake () {
-    m_turnMotor.getConfigurator().apply(turnMotorConfiguration());
-    m_driveMotor.getConfigurator().apply(driveMotorConfiguration());
-  }
+    public void configGroundIntake() {
+        m_turnMotor.getConfigurator().apply(turnMotorConfiguration());
+        m_driveMotor.getConfigurator().apply(driveMotorConfiguration());
+    }
 
-  public PositionVoltage goToRotationRequestion (double expectRotation) {
-    PositionVoltage turnRequest = new PositionVoltage(expectRotation);
-    SmartDashboard.putNumber("destination", expectRotation);
-    return turnRequest;
-  }
+    public PositionVoltage goToRotationRequestion(double expectRotation) {
+        PositionVoltage turnRequest = new PositionVoltage(expectRotation);
+        SmartDashboard.putNumber("destination", expectRotation);
+        return turnRequest;
+    }
 
-  public void expandGIntake () {
-    PositionVoltage request = goToRotationRequestion(GIntakeConstants.expandCCRotation);
-    System.out.println("expand request");
-    m_turnMotor.setControl(request);
-  }
+    public void expandGIntake() {
+        PositionVoltage request = goToRotationRequestion(GIntakeConstants.expandCCRotation);
+        System.out.println("expand request");
+        m_turnMotor.setControl(request);
+    }
 
-  public void retractGIntake () {
-    PositionVoltage request = goToRotationRequestion(GIntakeConstants.retractCCRotation);
-    System.out.println("retract request");
-    m_turnMotor.setControl(request);
-  }
+    public void retractGIntake() {
+        PositionVoltage request = goToRotationRequestion(GIntakeConstants.retractCCRotation);
+        System.out.println("retract request");
+        m_turnMotor.setControl(request);
+    }
 
-  public void zeroCC() {
-    m_CANcoder.setPosition(0);
-  }
+    public void zeroCC() {
+        m_CANcoder.setPosition(0);
+    }
 
-  public void startIntake () {
-    m_driveMotor.set(0.3);
-  }
+    public void startIntake() {
+        m_driveMotor.set(0.3);
+    }
 
-  public void stopIntake () {
-    m_driveMotor.set(0);
-  }
+    public void stopIntake() {
+        m_driveMotor.set(0);
+    }
 
-  public void reverseIntake() {
-    m_driveMotor.set(-0.1);
-  }
+    public void reverseIntake() {
+        m_driveMotor.set(-0.1);
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
 }
