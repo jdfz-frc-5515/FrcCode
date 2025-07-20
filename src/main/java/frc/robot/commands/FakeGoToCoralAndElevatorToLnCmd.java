d1cd809efc2b8af6ebeb1c3ddfeaac95f7bd0cf8
程式浪人 <raymondma529@foxmail.com>
2025-07-20 21:45:16 +0800

    codes

--- src/main/java/frc/robot/commands/FakeGoToCoralAndElevatorToLnCmd.java
+++ src/main/java/frc/robot/commands/FakeGoToCoralAndElevatorToLnCmd.java
@@ -1,8 +1,9 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.subsystems.Chassis.CommandSwerveDrivetrain;
 
-public class GoToCoralAndElevatorToLnCmd extends GoToCoralCmd {
+public class FakeGoToCoralAndElevatorToLnCmd extends GoToCoralCmd {
     private UpperSystem2025Cmd m_upSys;
     final double LIFTING_DISTANCE = 0.5; // meters, distance to lift the robot after reaching the coral
     boolean m_isMovingDone = false;
@@ -10,19 +11,22 @@ public class GoToCoralAndElevatorToLnCmd extends GoToCoralCmd {
     boolean m_isElevatorToLning = false;
     UpperSystem2025Cmd.STATE m_liftState = UpperSystem2025Cmd.STATE.NONE;
 
-    public GoToCoralAndElevatorToLnCmd(CommandSwerveDrivetrain subsystem, UpperSystem2025Cmd upSys) {
+    double m_fakeStartTime = 0;
+
+    public FakeGoToCoralAndElevatorToLnCmd(CommandSwerveDrivetrain subsystem, UpperSystem2025Cmd upSys) {
         super(subsystem, ()->false,  ()->false, true);
         m_upSys = upSys;
     }
 
     @Override
     public void initialize() {
-        super.initialize();
+        // super.initialize();
         m_isMovingDone = false;
         m_isElevatorToLnDone = false;
         m_isElevatorToLning = false;
         m_liftState = UpperSystem2025Cmd.STATE.NONE;
 
+        m_fakeStartTime = Timer.getFPGATimestamp();
         // if (!m_upSys.getIsCarryingCoral()) {
         //     m_isElevatorToLnDone = true;
         // }
@@ -30,12 +34,17 @@ public class GoToCoralAndElevatorToLnCmd extends GoToCoralCmd {
 
     @Override
     public void execute() {
-        if (!m_isMovingDone && m_subsystem.isAtTargetPose()) {
-            m_isMovingDone = true;
-            m_subsystem.customStopMoving(true);
-        }
-        else {
-            super.execute();
+        // super.execute(); 
+        // if (!m_isMovingDone && m_subsystem.isAtTargetPose()) {
+        //     m_isMovingDone = true;
+        //     m_subsystem.customStopMoving(true);
+        // }
+
+        if (!m_isMovingDone) {
+            double now = Timer.getFPGATimestamp();
+            if (now - m_fakeStartTime >= 2) {
+                m_isMovingDone = true;
+            }
         }
 
         if (!m_isElevatorToLnDone) {
@@ -46,7 +55,7 @@ public class GoToCoralAndElevatorToLnCmd extends GoToCoralCmd {
                     m_liftState = UpperSystem2025Cmd.STATE.NONE;
                 }
             }
-            else if (!m_isElevatorToLning && m_subsystem.getDistFromTargetPose() < LIFTING_DISTANCE) {
+            else if (!m_isElevatorToLning) {
                 m_liftState = m_upSys.setStateLn();
                 if (m_liftState != UpperSystem2025Cmd.STATE.NONE) {
                     m_isElevatorToLning = true;
